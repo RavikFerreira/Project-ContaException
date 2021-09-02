dedb761ec4ed2e7f13381ce9ea6a9636dcf00e42
RavikFerreira <ravikferreira.developer@gmail.com>
2021-09-02 13:24:45 -0300

    projeto contaException versão boa

---
+++ src/Model/Exceptions/Exceptions.java
@@ -0,0 +1,11 @@
+package Model.Exceptions;
+
+public class Exceptions extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public Exceptions(String msg){
+        super(msg);
+
+    }
+
+}
